8c13ddd75ec1564e8375651632c32b07b0552642
arun2985 <rated.star10@yahoo.com>
2021-04-21 21:16:09 +0530

    h

--- flaskmain.py
+++ flaskmain.py
@@ -16,7 +16,7 @@ headers = {
     'Content-Type': 'application/json'
 }
 vale = ""
-f = open("Arun.txt", "a")
+#f = open("Arun.txt", "a")
 
 
 def get_face_embeddings_from_image(image, convert_to_rgb=False):
@@ -42,48 +42,72 @@ def hello_world():
         #print(data["userId"])
         #print(data['userId'])
         #print(data['pic2'])
-        Img = data["pic2"]
-
+        Img = data["pic1"]
+        Img2 = data["pic2"]
         id = data["userId"]
+        print(Img)
         logo1 = imutils.url_to_image(Img)
+        logo2 = imutils.url_to_image(Img2)
         #print(logo2)
         #logo1 = imutils.resize(logo1, width=480)
         #print(logo2)
-        locations, encodings = get_face_embeddings_from_image(logo1)
-        unknown_face_encoding = encodings[0]
+
         E_id = r.get(id)
+
+        print("e_id",E_id)
+        if E_id ==None:
+            print("e_id is not found")
+            ocations, encodings = get_face_embeddings_from_image(logo1)
+            my_face_encoding = encodings[0]
+            r.set(id, my_face_encoding.tobytes())
+            E_id = r.get(id)
+        #except:
+        #    ocations, encodings = get_face_embeddings_from_image(logo1)
+        #    my_face_encoding = encodings[0]
+        #    r.set(id, my_face_encoding.tobytes())
+        #    E_id = r.get(id)
         #print(E_id)
         image = np.fromstring(E_id, dtype=np.float)
         decoded = image.reshape((128,))
+        locations, encodings = get_face_embeddings_from_image(logo2)
+        unknown_face_encoding = encodings[0]
         #dist = np.linalg.norm(decoded - unknown_face_encoding)
         result=face_recognition.compare_faces([decoded], unknown_face_encoding)
         face_distances = face_recognition.face_distance([decoded], unknown_face_encoding)
-        face_match_percentage = (1 - face_distances) * 100
-        for i, face_distance in enumerate(face_distances):
-            global vale
-            print("The test image has a distance of {:.2} from known image #{}".format(face_distance, i))
-            print("- comparing with a tolerance of 0.6? {}".format(face_distance < 0.6))
-            vale = np.round(face_match_percentage, 4)
-            print(vale)  # upto 4 decimal places
-        if result[0] == True:
-            d = json.dumps({"userId": id,  "code": 1, "faceStatus": "Face Matched", "value": str(vale)})
-            print(d)
+        vale = face_distances
+        if np.any(face_distances <= 0.6):
+            best_match_idx = np.argmin(face_distances)
+            print(best_match_idx)
+            d = json.dumps({"userId": id, "code": 0, "faceStatus": "Face Matched", "value": str(vale)})
         else:
-            d = json.dumps({"userId": id,  "code": 1, "faceStatus": "Face Not Matched", "value": str(vale)})
-            print(d)
+            name = None
+            d = json.dumps({"userId": id, "code": 1, "faceStatus": "Face Not Matched", "value": str(vale)})
+        #face_match_percentage = (1 - face_distances) * 100
+        #for i, face_distance in enumerate(face_distances):
+        #    global vale
+        #    print("The test image has a distance of {:.2} from known image #{}".format(face_distance, i))
+        #    print("- comparing with a tolerance of 0.6? {}".format(face_distance < 0.6))
+        #    vale = np.round(face_match_percentage, 4)
+        #    print(vale)  # upto 4 decimal places
+        #if result[0] == True:
+        #   d = json.dumps({"userId": id,  "code": 1, "faceStatus": "Face Matched", "value": str(vale)})
+        #    print(d)
+        #else:
+        #    d = json.dumps({"userId": id,  "code": 1, "faceStatus": "Face Not Matched", "value": str(vale)})
+        #    print(d)
     except Exception as e:
         print(e)
         d = json.dumps({"userId": id, "code": 3, "faceStatus": "Face NOT Detected","value": str(vale)})
     #url = 'http://127.0.0.1:8000/'
     #d=json.dumps({"userId": id, "faceStatus": result2})
     print(d)
-    f.write(d + '\n')
+    #f.write(d + '\n')
     from requests.packages.urllib3.exceptions import InsecureRequestWarning
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     re = requests.post(url, data=d, verify=False)
     print(re)
     return d
-f.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5009, debug=True)
